c2fff5483dd896889b964922c37d761a6b08fa7d
Andreas Ntalakas <antalakas@gmail.com>
2014-02-13 22:07:41 +0100

    works, as separate, manual steps

--- README.md
+++ README.md
@@ -6,19 +6,52 @@ Dart DDP Client
 An experiment, trying to enable dart to subscribe to meteor collections using ddp protocol.
 
 Based on:
-
+--------
 https://github.com/oortcloud/node-ddp-client
 https://github.com/dart-lang/dart-samples/tree/master/web/html5/websockets/basics
 https://github.com/meteor/meteor/blob/devel/packages/livedata/DDP.md
 
-What works:
+You need the posts collection from microscope sample application.
 
-Connect to web socket
-Connection to the server using ddp, server replies with server id
+You need also the ddp-analyzer:
+http://meteorhacks.com/discover-meteor-ddp-in-realtime.html
+
+What works:
+----------
+1. Connect to web socket
+
+2. Login(plain text)/Connection to the ddp server
+
+3. Subscribe/Unsubscribe to collections.
+
 
 To do:
+-----
+1. Handling collection(s) retrieval on the client
 
-Subscribe/Unsubscribe to collections.
-Handling collection(s) retrieval on the client
-Calling server side methods
-Integration with the security system
+2. Calling server side methods
+
+
+Sample output:
+--------------
+Connecting to webSocket
+Connected to webSocket
+OUT: {"msg":"connect", "version":"pre1", "support":"['pre1']"}
+Received message: {"server_id":"0"}
+Received message: {"msg":"connected","session":"S6CHc3FsrF5SbZ6MS"}
+OUT: {"msg":"sub", "id":"RuEMs6rwo5PdCv9qJ", "name":"meteor_autoupdate_clientVersions", "params":[]}
+Received message: {"msg":"added","collection":"meteor_autoupdate_clientVersions","id":"55f6b5c711a158082fe91949041fa4e1d53af5fc","fields":{"current":true}}
+Received message: {"msg":"ready","subs":["RuEMs6rwo5PdCv9qJ"]}
+OUT: {"msg":"sub", "id":"NnJSdQsnDf5o8HLkE", "name":"meteor.loginServiceConfiguration", "params":[]}
+Received message: {"msg":"ready","subs":["NnJSdQsnDf5o8HLkE"]}
+OUT: {"msg":"sub", "id":"iQjpD7mYfm3D4ybEF", "name":"posts", "params":[]}
+Received message: {"msg":"added","collection":"posts","id":"WAy3TbFzujvmq4xyB","fields":{"title":"Introducing Telescope","author":"Sacha Greif","url":"http://sachagreif.com/introducing-telescope/"}}
+Received message: {"msg":"added","collection":"posts","id":"GEoTK9wbyc4jBvaqJ","fields":{"title":"Meteor","author":"Tom Coleman","url":"http://meteor.com"}}
+Received message: {"msg":"added","collection":"posts","id":"o2tFc64QJxX42Nmwz","fields":{"title":"The Meteor Book","author":"Tom Coleman","url":"http://themeteorbook.com"}}
+Received message: {"msg":"ready","subs":["iQjpD7mYfm3D4ybEF"]}
+OUT: {"msg":"unsub", "id":"iQjpD7mYfm3D4ybEF"}
+Received message: {"msg":"removed","collection":"posts","id":"WAy3TbFzujvmq4xyB"}
+Received message: {"msg":"removed","collection":"posts","id":"GEoTK9wbyc4jBvaqJ"}
+Received message: {"msg":"removed","collection":"posts","id":"o2tFc64QJxX42Nmwz"}
+Received message: {"msg":"nosub","id":"iQjpD7mYfm3D4ybEF"}
+webSocket closed
--- web/ddpclient.dart
+++ web/ddpclient.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 import 'dart:async';
-import 'dart:js';
+//import 'dart:js';
+import 'dart:convert';
 
 class DDPClient {
   
@@ -21,6 +22,18 @@ class DDPClient {
     
     var closeButton = querySelector("#close");
     closeButton.onClick.listen((ev) => closeClick(ev));
+    
+    var subscribeAutoUpdateButton = querySelector("#subscribeAutoUpdate");
+    subscribeAutoUpdateButton.onClick.listen((ev) => subscribeAutoUpdateClick(ev));
+    
+    var subscribeLoginServiceButton = querySelector("#subscribeLoginService");
+    subscribeLoginServiceButton.onClick.listen((ev) => subscribeLoginServiceClick(ev));
+    
+    var subscribeButton = querySelector("#subscribe");
+    subscribeButton.onClick.listen((ev) => subscribeClick(ev));
+    
+    var unsubscribeButton = querySelector("#unsubscribe");
+    unsubscribeButton.onClick.listen((ev) => unsubscribeClick(ev));
   }
  
   void connectClick(ev) {
@@ -29,6 +42,28 @@ class DDPClient {
 
   void closeClick(ev) {
     close(); 
+  }
+  
+  void subscribeClick(ev) {
+    subscribe(); 
+  }
+  
+  void subscribeAutoUpdateClick(ev) {
+    // 1  OUT  6  {"msg":"sub","id":"RuEMs6rwo5PdCv9qJ","name":"meteor_autoupdate_clientVersions","params":[]}
+    var autoupdatePayload = '{"msg":"sub", "id":"RuEMs6rwo5PdCv9qJ", "name":"meteor_autoupdate_clientVersions", "params":[]}';
+    outputMsg('OUT: $autoupdatePayload');
+    send(autoupdatePayload);
+  }
+  
+  void subscribeLoginServiceClick(ev) {
+    // 1  OUT  11  {"msg":"sub","id":"NnJSdQsnDf5o8HLkE","name":"meteor.loginServiceConfiguration","params":[]}
+    var loginServicePayload = '{"msg":"sub", "id":"NnJSdQsnDf5o8HLkE", "name":"meteor.loginServiceConfiguration", "params":[]}';
+    outputMsg('OUT: $loginServicePayload');
+    send(loginServicePayload);
+  }
+
+  void unsubscribeClick(ev) {
+    unsubscribe(); 
   }
   
   void connect([int retrySeconds = 2]) {
@@ -48,7 +83,10 @@ class DDPClient {
     
     webSocket.onOpen.listen((e) {
       outputMsg('Connected to webSocket');
-      var connectionPayload = new JsObject(context['connectionPayload']);
+     
+      // 1  OUT  0  {"msg":"connect","version":"pre1","support":["pre1"]}
+      var connectionPayload = '{"msg":"connect", "version":"pre1", "support":"[\'pre1\']"}';
+      outputMsg('OUT: $connectionPayload');
       send(connectionPayload);
     });
 
@@ -72,8 +110,6 @@ class DDPClient {
     webSocket.send(data);
   }
   
-  // handle a message from the server
-  // data
   void handleMessage(var data) {
   }
   
@@ -81,33 +117,20 @@ class DDPClient {
     webSocket.close();
   }
   
-  // call a method on the server,
-  //
-  // callback = function(err, result)
-  // name, params, callback
   void callServerMethod() {
-    //var id = self._nextId();
-
-    //if (callback)
-    //  callbacks[id] = callback;
-
-    //send({msg: 'method', id: id, method: name, params: params});
   }
   
-  // open a subscription on the server, callback should handle on ready and nosub
-  // name, params, callback
-  void subscribe() {
-    //var id = self._nextId();
-
-    //if (callback)
-    //  callbacks[id] = callback;
-
-    //send({msg: 'sub', id: id, name: name, params: params});
+  void subscribe() {    
+    //  1  OUT  102  {"msg":"sub","id":"iQjpD7mYfm3D4ybEF","name":"posts","params":[]}
+    var subscriptionPayload = '{"msg":"sub", "id":"iQjpD7mYfm3D4ybEF", "name":"posts", "params":[]}';
+    outputMsg('OUT: $subscriptionPayload');
+    send(subscriptionPayload);
   }
   
-  // id
   void unsubscribe() {
-    // send({msg: 'unsub', id: id});
+    var endSubscriptionPayload = '{"msg":"unsub", "id":"iQjpD7mYfm3D4ybEF"}';
+    outputMsg('OUT: $endSubscriptionPayload');
+    send(endSubscriptionPayload);
   }
 }
 
@@ -124,3 +147,29 @@ void main() {
   
   DDPClient ddpClient = new DDPClient(); 
 }
+
+/*
+DDP Proxy Started on port: 3030
+===============================
+Export following env. variables and start your meteor app
+  export DDP_DEFAULT_CONNECTION_URL=http://localhost:3030
+  meteor
+
+New Client: [1]
+
+ 1  OUT  0  {"msg":"connect","version":"pre1","support":["pre1"]}
+ 1  OUT  6  {"msg":"sub","id":"RuEMs6rwo5PdCv9qJ","name":"meteor_autoupdate_clientVersions","params":[]}
+ 1  OUT  11  {"msg":"sub","id":"NnJSdQsnDf5o8HLkE","name":"meteor.loginServiceConfiguration","params":[]}
+ 1  IN   6  {"server_id":"0"}
+ 1  IN   18  {"msg":"connected","session":"fh6asDzj785ALpDdD"}
+ 1  IN   5  {"msg":"added","collection":"meteor_autoupdate_clientVersions","id":"55f6b5c711a158082fe91949041fa4e1d53af5fc","fields":{"current":true}}
+ 1  IN   0  {"msg":"ready","subs":["RuEMs6rwo5PdCv9qJ"]}
+ 1  IN   18  {"msg":"ready","subs":["NnJSdQsnDf5o8HLkE"]}
+ 1  OUT  102  {"msg":"sub","id":"iQjpD7mYfm3D4ybEF","name":"posts","params":[]}
+ 1  IN   10  {"msg":"added","collection":"posts","id":"WAy3TbFzujvmq4xyB","fields":{"title":"Introducing Telescope","author":"Sacha Greif","url":"http://sachagreif.com/introducing-telescope/"}}
+ 1  IN   0  {"msg":"added","collection":"posts","id":"GEoTK9wbyc4jBvaqJ","fields":{"title":"Meteor","author":"Tom Coleman","url":"http://meteor.com"}}
+ 1  IN   1  {"msg":"added","collection":"posts","id":"o2tFc64QJxX42Nmwz","fields":{"title":"The Meteor Book","author":"Tom Coleman","url":"http://themeteorbook.com"}}
+ 1  IN   0  {"msg":"added","collection":"posts","id":"Nqsnu7E4ako5rP8KG","fields":{"url":"www.contra.gr","title":"Contra","message":"contra gr"}}
+ 1  IN   0  {"msg":"added","collection":"posts","id":"MvhfvGh9D99qyvhpj","fields":{"url":"www.redplanet.gr","title":"RedPlanet","message":"RedPlanet gr"}}
+ 1  IN   0  {"msg":"ready","subs":["iQjpD7mYfm3D4ybEF"]}
+ */
